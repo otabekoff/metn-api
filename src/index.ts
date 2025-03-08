3f9685f1641e1e33e1562a2c4f5bec0c1c90e2a3
Otabek <sadiridinovotabek@gmail.com>
2025-03-08 19:20:57 +0500

    MongoDB setup & creating User model

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose, { mongo } from "mongoose";
 
 const app = express();
 
@@ -29,3 +30,27 @@ app.get("/", (req, res) => {
 server.listen(8080, () => {
   console.log("Server started on http://localhost:8080/");
 });
+
+const MONGO_URL =
+  "mongodb+srv://bekky:fv4zMnhKXGmEbV5J@cluster0.poqkapp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose
+  .connect(
+    MONGO_URL
+    // , {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    // }
+  )
+  .then(() => console.log("✅ Successfully connected to MongoDB"))
+  .catch((error) => console.error("❌ MongoDB connection error:", error));
+
+mongoose.connection.on("error", (error: Error) => console.error(error));
+mongoose.connection.on("open", () => console.log("Connected to MongoDB"));
+mongoose.connection.on("close", () => console.log("Disconnected from MongoDB"));
+mongoose.connection.on("reconnected", () =>
+  console.log("Reconnected to MongoDB")
+);
