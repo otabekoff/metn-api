6ec0929c61ca8c60d4241869bd847304559cf114
Otabek <sadiridinovotabek@gmail.com>
2025-03-08 09:12:07 +0500

    Setup Express server + Small changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# metn-api
-METN (MongoDB, Express, TypeScript and NodeJS API) project.
+# METN API
+MongoDB, Express, TypeScript and NodeJS API project.
+
+This repository contains the REST API project source code made with technologies listed above.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello, Node!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    // origin: 'http://localhost:3000'
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = http.createServer(app);
+
+// Just a simple route to test the server
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(8080, () => {
+  console.log("Server started on http://localhost:8080/");
+});
